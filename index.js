5fe4720b78f5718b75de85fcd7ad81876373d1ce
yangkuo1 <yangkuo1@100tal.com>
2018-11-12 10:31:09 +0800

    static

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Koa = require('koa');
+const static = require('koa-static');
+const path = require('path');
 const app = new Koa();
-app.use(async ctx => {
-    ctx.body = 'Hello world'
+app.use(static(path.resolve('dist')));
+app.listen(3000, () => {
+    console.log('success')
 })
-app.listen(3000)
